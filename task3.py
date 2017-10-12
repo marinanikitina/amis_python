9e9fbfcea6fc69d980be83f91765487a64e5b7f6
Marina Nikitina <31968682+marinanikitina@users.noreply.github.com>
2017-10-13 00:23:01 +0300

    Homework_1
    Виконала Нікітіна М.А. студентка КМ-71
    Тема; "Поняття змінної та типу даних у Python"

---
+++ task3.py
@@ -0,0 +1,3 @@
+a = input("Напишить своє ім'я - ")
+b = print("Hello,",a,'!')
+c = input()
---
+++ task5.py
@@ -0,0 +1,5 @@
+x = int(input("Введіть число - "))
+m = x - 1
+n = x + 1
+print("The next number for the number",x,"is",n,"\n","The previous number for the number",x,"is",m)
+input()
